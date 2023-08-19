30c5104189cbd6351bf79803e7acaff3b983551b
Siamak Rasouli <empire_of_web@yahoo.com>
2023-08-19 11:50:10 +0330

    ADD redirect()

--- System/helpers/uri.php
+++ System/helpers/uri.php
@@ -6,3 +6,7 @@ function base_url($url = '') {
 function parseURI() {
     return explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 }
+function redirect($url, $response_code = 301) {
+    header("Location: " . $url, true, $response_code);
+    exit();
+}
